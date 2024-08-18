f0864fa1bb609aef7adb317c3a0587225813b8c3
Felix Boucher <boucherlfg@gmail.com>
2024-08-17 22:00:24 -0400

    add art and bugfixes

--- Mouton/Assets/Scripts/AnimationScript.cs
+++ Mouton/Assets/Scripts/AnimationScript.cs
@@ -9,9 +9,13 @@ public class AnimationScript : MonoBehaviour
     float lastVelocityX = 1;
     private Animator animator;
     private HandScript hand;
+    private MoveScript move;
+    private JumpScript jump;
     void Start() {
         animator = GetComponent<Animator>();
         hand = FindObjectOfType<HandScript>();
+        move = FindObjectOfType<MoveScript>();
+        jump = move.GetComponent<JumpScript>();
     }
     // Update is called once per frame
     void Update()
@@ -21,9 +25,14 @@ public class AnimationScript : MonoBehaviour
         transform.localScale = Vector3.right * lastVelocityX + Vector3.up  + Vector3.forward;
 
         string action = "";
+        
+        if(jump.isJumping) action = "Jump" + (_rigidBody.velocity.y < 0 ? "Down" : "Up");
+        else if(Mathf.Abs(_rigidBody.velocity.x) > 0.1f) action = "Walk";
+        else action = "Idle";
+
         if(hand.carried) action += "Hold";
-        if(Mathf.Abs(_rigidBody.velocity.x) > 0.1f) action = "Walk" + action;
-        else action = "Idle" + action;
+
+        if(move.YAxisMove) action = "Climb" + (_rigidBody.velocity.magnitude < 0.1f ? "Idle" : "");
 
         animator.Play(action);
     }
--- Mouton/Assets/Trampoline.cs
+++ Mouton/Assets/Trampoline.cs
@@ -6,18 +6,24 @@ public class Trampoline : MonoBehaviour
 {
     public float jumpHeight = 20;
     void OnCollisionEnter2D(Collision2D collision) {
-        if(!collision.gameObject.TryGetComponent(out FootScript foot)) return;
-        if((transform.position - foot.transform.position).y < 0.1f) return;
+        if(!collision.gameObject.TryGetComponent(out FootScript foot) 
+            && !collision.gameObject.GetComponent<Ingredient>() 
+            && !collision.gameObject.GetComponent<FoodScript>()) return;
+        if((transform.position - collision.transform.position).y < 0.1f) return;
 
         Debug.Log("collision works");
         FindObjectOfType<JumpScript>().GetComponent<Rigidbody2D>().velocity += Vector2.up;
+        GetComponent<Animator>().Play("Trampoline");
     }
     void OnTriggerEnter2D(Collider2D collision) {
-        if(!collision.gameObject.TryGetComponent(out FootScript foot)) return;
-        var delta = transform.position - foot.transform.position;
+        if(!collision.gameObject.TryGetComponent(out FootScript foot) 
+            && !collision.gameObject.GetComponent<Ingredient>() 
+            && !collision.gameObject.GetComponent<FoodScript>()) return;
+        var delta = transform.position - collision.transform.position;
         if(delta.y > 0.1f) return;
 
         Debug.Log("trigger works");
         FindObjectOfType<JumpScript>().GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpHeight;
+        GetComponent<Animator>().Play("Trampoline");
     }
 }
